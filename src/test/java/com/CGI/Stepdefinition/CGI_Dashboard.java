2c2fc15570d0322727d815e29edd0d95601663e1
thiru0211 <0211thiru@gmail.com>
2024-10-05 13:22:05 +0530

    Latest commit

--- src/test/java/com/CGI/Stepdefinition/CGI_Dashboard.java
+++ src/test/java/com/CGI/Stepdefinition/CGI_Dashboard.java
@@ -36,7 +36,7 @@ public class CGI_Dashboard {
 	public void to_check_Dashboard_user_is_navigating_to_cgi_url_is(String URL) {
 		System.setProperty("webdriver.chrome.driver", ".\\Driver\\chromedriver.exe");
 		ChromeOptions option = new ChromeOptions();
-		option.addArguments("--headless=old");
+//		option.addArguments("--headless=old");
 		driver = new ChromeDriver(option);
 		driver.manage().window().maximize();
 		driver.get(URL);
